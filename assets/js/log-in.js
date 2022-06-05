f4c63c433d077f17188ea89bbf8f76cb9a7e1a59
VongNgaGiun <hycj221@gmail.com>
2022-06-05 15:49:08 +0800

    log in js default

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -1,13 +1,23 @@
 function openTab(event, tabName) {
+  // Declare all variables
   var i, tabContent, tabLink;
+
+  // Get all elements with class="tabcontent" and hide them
   tabContent = document.getElementsByClassName('tabContent');
   for (i = 0; i < tabContent.length; i++) {
     tabContent[i].style.display = 'none';
   }
+
+  // Get all elements with class="tablinks" and remove the class "active"
   tabLink = document.getElementsByClassName('tabLink');
   for (i = 0; i < tabLink.length; i++) {
     tabLink[i].className = tabLink[i].className.replace(' active', '');
   }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = 'block';
   event.currentTarget.className += ' active';
 }
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById('defaultOpen').click();
