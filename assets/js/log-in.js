a00563a3d0043f30a0550a537a47c61daa20a5b6
VongNgaGiun <hycj221@gmail.com>
2022-06-07 12:03:10 +0800

    css

--- assets/js/log-in.js
+++ assets/js/log-in.js
@@ -2,13 +2,13 @@ function openTab(event, tabName) {
   // Declare all variables
   var i, tabContent, tabLink;
 
-  // Get all elements with class="tabcontent" and hide them
+  // Get all elements with class="tabContent" and hide them
   tabContent = document.getElementsByClassName('tabContent');
   for (i = 0; i < tabContent.length; i++) {
     tabContent[i].style.display = 'none';
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
+  // Get all elements with class="tabLink" and remove the class "active"
   tabLink = document.getElementsByClassName('tabLink');
   for (i = 0; i < tabLink.length; i++) {
     tabLink[i].className = tabLink[i].className.replace(' active', '');
